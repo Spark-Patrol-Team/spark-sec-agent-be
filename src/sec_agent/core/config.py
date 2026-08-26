5c05d61d4e69d9143e86a5298174e4f5b89805e5
becky200605 <lidehua0821@gmail.com>
2026-08-26 20:28:13 +0800

    feat:增加运营看板CORS默认来源

--- src/sec_agent/core/config.py
+++ src/sec_agent/core/config.py
@@ -13,6 +13,8 @@ DEFAULT_CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
 ]
 
 
